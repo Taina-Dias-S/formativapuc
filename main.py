d43a431bd672c5ec67135dc8fd1167379b9a8ef5
Taina-Dias-S <tainasilva2501@gmail.com>
2026-04-18 17:21:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/testee")
+@app.get("/testando")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0,50)}
 
